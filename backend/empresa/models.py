258b96e98b4adc04ddec5b96d972fa9997e5f353
NycolasLuiz <nycolasluizsamuel@gmail.com>
2026-08-23 19:27:55 -0300

    feat: cria models da entidade empresa

--- backend/empresa/models.py
+++ backend/empresa/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Empresa (models.Model):
+    cnpj = models.CharField(max_length=18, primary_key=True)
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100, unique=True)
+    data_fundacao =models.DateField(("Data criação"), auto_now=False, auto_now_add=False)
+  
+    
+    def __str__(self):
+        return self.nome    
+    
